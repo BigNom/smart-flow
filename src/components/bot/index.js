84fc09fda0db45dd4bed57a13a1852b6ee32d8e0
bignom <simon.biddle@live.com>
2017-11-18 16:03:14 +0800

    added bot

---
+++ src/components/bot/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bot = () => (
+  <iframe
+  width="350"
+  height="430"
+  src="https://console.dialogflow.com/api-client/demo/embedded/e9243e61-82cb-451a-a20b-8528b1bdbf15">
+</iframe>
+)
+
+export default Bot
